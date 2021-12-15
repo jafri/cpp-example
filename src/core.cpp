231d0a1d67883052e3ae2b48ecaac1b197a5e01c
Syed Jafri <syed.jafri2@ucalgary.ca>
2021-12-14 18:36:28 -0700

    latest

--- src/core.cpp
+++ src/core.cpp
@@ -1,23 +1,33 @@
 #include <tuple>
 #include "core.h"
+#include <iostream> 
+#include <math.h>       /* ceil */
 
-int add(int a, int b) {
-  return a + b;
-}
+uint64_t compute_transfer(uint128_t token1, uint128_t token2, uint128_t quantity, uint128_t exchange_fee)
+{
+  const uint128_t coefficient = ceil((token1 * token2 * 1.0) / (token1 + quantity));
+  uint128_t transfer = token2 - coefficient;
 
-Vector2::Vector2(float x, float y) {
-  set(x, y);
-}
+  std::cout << "Token 1: ";
+  print_u128_u(token1);
+  std::cout << std::endl << "Token 2: ";
+  print_u128_u(token2);
+  std::cout << std::endl << "Quantity: ";
+  print_u128_u(quantity);
+  std::cout << std::endl << "Exchange fee: ";
+  print_u128_u(exchange_fee);
+  std::cout << std::endl << "Coefficient: ";
+  print_u128_u(coefficient);
+  std::cout << std::endl << "Transfer 1: ";
+  print_u128_u(transfer);
 
-Vector2::~Vector2() {
+  transfer -= (transfer * exchange_fee) / FEE_FACTOR;
 
-}
 
-void Vector2::set(float x, float y) {
-  m_x = x;
-  m_y = y;
-}
+  std::cout << std::endl << "Transfer 2: ";
+  print_u128_u(transfer);
 
-std::tuple<float, float> Vector2::get() {
-  return std::tuple<float, float> (m_x, m_y);
+  std::cout << std::endl << "Transfer 3: " << static_cast<uint64_t>(transfer) << std::endl;
+
+  return static_cast<uint64_t>(transfer);
 }
--- src/core.h
+++ src/core.h
@@ -2,19 +2,40 @@
 #define CORE_H
 
 #include <tuple>
+#include <stdio.h>
+#include <inttypes.h>
 
-int add(int a, int b);
+using uint128_t = __uint128_t;
 
-class Vector2 {
-  private:
-    float m_x;
-    float m_y;
+static constexpr uint128_t FEE_PRECISION = 100;
+static constexpr uint128_t FEE_FACTOR    = FEE_PRECISION * 100;
 
-  public:
-    Vector2(float x, float y);
-    ~Vector2();
-    void set(float x, float y);
-    std::tuple<float, float> get();
-};
+
+/*      UINT64_MAX 18446744073709551615ULL */
+#define P10_UINT64 10000000000000000000ULL   /* 19 zeroes */
+#define E10_UINT64 19
+
+#define STRINGIZER(x)   # x
+#define TO_STRING(x)    STRINGIZER(x)
+
+static inline int print_u128_u(uint128_t u128)
+{
+    int rc;
+    if (u128 > UINT64_MAX)
+    {
+        uint128_t leading  = u128 / P10_UINT64;
+        uint64_t  trailing = u128 % P10_UINT64;
+        rc = print_u128_u(leading);
+        rc += printf("%." TO_STRING(E10_UINT64) PRIu64, trailing);
+    }
+    else
+    {
+        uint64_t u64 = u128;
+        rc = printf("%" PRIu64, u64);
+    }
+    return rc;
+}
+
+uint64_t compute_transfer(uint128_t token1, uint128_t token2, uint128_t quantity, uint128_t exchange_fee);
 
 #endif
